b2d2d7128cf81d66cb1b81f62230d2989258543d
1steveee <truckersteve781@gmail.com>
2023-07-16 21:13:33 -0400

    updated github actions yml file to include username and password

--- src/test/java/data/TokenCreds.java
+++ src/test/java/data/TokenCreds.java
@@ -5,7 +5,10 @@ import lombok.Data;
 
 @Data
 public class TokenCreds {
-    private String username = System.getProperty("username");
-    private String password = System.getProperty("password");
+//    private String username = System.getProperty("username");
+//    private String password = System.getProperty("password");
+
+    private String username = "admin";
+    private String password = "password123";
 
 }
